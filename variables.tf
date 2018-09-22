46b76d8957b22f98b486856a6331f9fe9d61710e
Matthias Malzahn <matthias.malzahn@comlineag.de>
2018-09-22 13:20:21 +0200

    add new Variables

--- variables.tf
+++ variables.tf
@@ -1,9 +1,34 @@
+variable "tag_responsibel" {
+  description = "Wer ist fuer die Resourcen zustaendig"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "subnet_id" {}
+
+variable "pingable" {
+  default = true
+}
+variable "project_name" {
+  default = "Prj123"
+}
+
+variable "resource_prefix" {}
+variable "resource_name" {}
+
+variable "resource_suffix" {
+  default = ""
+}
+
+variable "resource_tags" {
+  type = "map"
+}
+
 # variable "aws_region" {
 #   default     = "eu-west-1"
 #   description = "AWS Region fuer das Deployment"
-# }
-# variable "tag_responsibel" {
-#   description = "Wer ist fuer die Resourcen zustaendig"
 # }
 # variable "aws_key_name" {}
 # variable "project_name" {}
@@ -14,14 +39,8 @@
 #   default = 60
 # }
 
+
 # variable "debug_on" {
 #   default = true
 # }
 
-# variable "dockercluster_instance_type" {
-#   default = "t2.micro"
-# }
-
-# variable "dockercluster_deploy" {
-#   default = false
-# }
