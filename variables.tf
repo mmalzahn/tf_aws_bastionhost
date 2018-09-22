406709e57352127b9a2555328b8fbbf10987990d
Matthias Malzahn <matthias.malzahn@comlineag.de>
2018-09-22 14:05:15 +0200

    new module variables

--- variables.tf
+++ variables.tf
@@ -1,46 +1,50 @@
-variable "tag_responsibel" {
-  description = "Wer ist fuer die Resourcen zustaendig"
-}
-
 variable "instance_type" {
   default = "t2.micro"
 }
+variable "vpc_id" {}
 
 variable "subnet_id" {}
 
 variable "pingable" {
   default = true
 }
+
 variable "project_name" {
   default = "Prj123"
 }
 
-variable "resource_prefix" {}
-variable "resource_name" {}
+variable "resource_prefix" {
+  default="prefix_"}
+variable "resource_name" {
+  default=""
+}
 
 variable "resource_suffix" {
-  default = ""
+  default = "_suffix"
 }
 
 variable "resource_tags" {
   type = "map"
+  default={}
+}
+
+variable "debug_on" {
+  default = true
+}
+
+variable "aws_key_name" {
+  default = ""
 }
 
 # variable "aws_region" {
 #   default     = "eu-west-1"
 #   description = "AWS Region fuer das Deployment"
 # }
-# variable "aws_key_name" {}
 # variable "project_name" {}
 # variable "optimal_design" {
 #   default = false
 # }
 # variable "laufzeit_tage" {
 #   default = 60
-# }
-
-
-# variable "debug_on" {
-#   default = true
 # }
 
